20f44e6dd72a1fc78fee8be70b983c8f326cb5c5
Имя <vitya.klyucherov@mail.ru>
2022-12-15 17:04:17 +0300

    Дз. Строки

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Строки");
+        team1();
+        team2();
+        team3();
+
+    }
+
+    public static void team1() {
+
+        System.out.println("Дз. Строки");
+
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+    }
+
+    public static void team2() {
+
+        System.out.println("Задача 2");
+
+        String firstName = "ivan ";
+        firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+        String middleName = "ivanovich ";
+        middleName = middleName.substring(0, 1).toUpperCase() + middleName.substring(1);
+        String lastName = "ivanov ";
+        lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+        String fullName = lastName + firstName + middleName;
+        System.out.println( "Данные ФИО сотрудника для заполнения отчета  " + fullName);
+    }
+    public static void team3() {
+
+        System.out.println("Задача 3");
+
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
